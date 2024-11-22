b6e46e2f1f62c57714e7e448dba29a2d8c36a70e
Exc0mmun1cad0 <fauxmen4@gmail.com>
2024-11-23 00:15:07 +0300

    added config loading

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -1,5 +1,25 @@
 package main
 
+import (
+	"fmt"
+	"url-shortener/internal/config"
+)
+
 func main() {
-	
+	config := config.MustLoad()
+	fmt.Println("loaded configuration")
+	_ = config
+
+	// TODO: logger initialization
+
+	// TODO: storage initialization
+
+	// TODO: cache initialization
+
+	// TODO: creating router
+	// TODO: registering handlers
+	// TODO: adding middlewares to router
+
+	// TODO: HTTP server initialization
+	// TODO: graceful shutdownm
 }
