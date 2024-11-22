cd8072eb6fd933e9c1869f92f9959eb68d95f447
Exc0mmun1cad0 <fauxmen4@gmail.com>
2024-11-23 00:42:43 +0300

    added logger initalization

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -2,13 +2,28 @@ package main
 
 import (
 	"fmt"
+	"log/slog"
+	"os"
 	"url-shortener/internal/config"
+	"url-shortener/pkg/badaslog"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
 )
 
 func main() {
 	config := config.MustLoad()
-	fmt.Println("loaded configuration")
-	_ = config
+
+	logger := NewLogger(config.Env)
+
+	logger.Info(
+		"starting url-shortener service",
+		slog.String("env", config.Env),
+		slog.String("address", fmt.Sprintf("%s:%d", config.HTTPServer.Host, config.HTTPServer.Port)),
+	)
 
 	// TODO: logger initialization
 
@@ -22,4 +37,36 @@ func main() {
 
 	// TODO: HTTP server initialization
 	// TODO: graceful shutdownm
+}
+
+func NewLogger(env string) *slog.Logger {
+	var logger *slog.Logger
+
+	switch env {
+	case envLocal:
+		logger = setupBadaSlogger()
+	case envDev:
+		logger = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		logger = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	default:
+		logger = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+
+	return logger
+}
+
+func setupBadaSlogger() *slog.Logger {
+	var logger *slog.Logger
+
+	opts := &slog.HandlerOptions{Level: slog.LevelDebug}
+	logger = slog.New(badaslog.NewHandler(opts))
+
+	return logger
 }
